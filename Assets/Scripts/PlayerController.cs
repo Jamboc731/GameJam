252820e73650a8c4d7911ff783a94118ae5682ea
JamieCovell <jamiec.covellj@gmail.com>
2018-10-16 00:25:11 +0100

    FixedPlayerController
    Controller only moved in local space. now fixed so it moves with respect to local rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     CharacterController cc;
-    Vector3 movement;
+    Vector3 movement = Vector3.zero;
     GameObject rotatable;
     public float gravity;
     public float lookSensitivity;
     Vector3 lookRot;
     Vector3 toRotate;
+    Vector3 forward;
 
     private void Start ()
     {
@@ -23,18 +24,24 @@ public class PlayerController : MonoBehaviour {
 
     private void Update ()
     {
-
+        /*
         movement.x = Input.GetAxisRaw ("Horizontal");
         movement.y = -gravity * Time.deltaTime;
         movement.z = Input.GetAxisRaw ("Vertical");
+        */
 
+        movement = transform.forward * Input.GetAxisRaw ("Vertical");
+        movement += transform.right * Input.GetAxisRaw ("Horizontal");
         movement = movement.normalized;
+        //forward = transform.forward;
         movement *= speed * Time.deltaTime;
         cc.Move (movement);
 
-        lookRot = new Vector3 (-Input.GetAxis ("Mouse Y"), Input.GetAxis ("Mouse X"), 0) * lookSensitivity;
+        lookRot = new Vector3 (-Input.GetAxis ("Mouse Y"), 0, 0) * lookSensitivity;
 
         rotatable.transform.Rotate (lookRot);
+
+        transform.Rotate (0, Input.GetAxis("Mouse X") * lookSensitivity, 0);
 
         toRotate = rotatable.transform.rotation.eulerAngles;
 
