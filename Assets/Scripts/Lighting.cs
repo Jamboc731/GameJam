996d66f6500ed97254452433d23cacc29584b7a7
JamieJessop <jamiej-2000@hotmail.com>
2018-10-16 18:10:17 +0100

    Minor changes
    Got the lighting to update when in runtime, previously it would switch back to skybox, and the ambient light would be black, and looked un natural, fixed now.

---
+++ Assets/Scripts/Lighting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("UpdateLighting", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Trilight;
+    }
+}
