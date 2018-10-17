ac2a778f7d835542f1998646d832a2fe918669ef
Jamie Jessop <jamiej-2000@hotmail.com>
2018-10-18 00:23:01 +0100

    Sand added, and A LOT OF COOL STUFF

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject Skel;
+    
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawn", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void spawn () {
+        GameObject instance;
+        GameObject[] Bones;
+        instance = Instantiate(Skel, transform.position, transform.rotation);
+        Bones = instance.GetComponentsInChildren<GameObject>();
+        Destroy(instance, 5f);
+
+        for (int i = 0; i < Bones.Length; i++)
+        {
+            Bones[i].GetComponent<Rigidbody>().AddForce(transform.forward * 5000);
+            Bones[i].GetComponent<Renderer>().material.SetFloat("_Dissolve", 1f);
+        }
+            
+       
+	}
+}
